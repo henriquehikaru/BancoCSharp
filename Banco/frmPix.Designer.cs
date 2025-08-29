af51c41acf35ae648c95e83fcbc77901e3f967e0
henriquehikaru <henrique.hikaru@unesp.br>
2025-08-28 21:28:11 -0300

    programacao do frmPix

--- Banco/frmPix.Designer.cs
+++ Banco/frmPix.Designer.cs
@@ -39,19 +39,20 @@
             this.label8 = new System.Windows.Forms.Label();
             this.txtPagador = new System.Windows.Forms.TextBox();
             this.txtRemetente = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.txtContaPagador = new System.Windows.Forms.TextBox();
+            this.txtContaRemetente = new System.Windows.Forms.TextBox();
+            this.numValorTransferencia = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxMetodoPagamento = new System.Windows.Forms.ComboBox();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numValorTransferencia)).BeginInit();
             this.SuspendLayout();
             // 
             // btnTransferir
             // 
-            this.btnTransferir.Location = new System.Drawing.Point(177, 330);
+            this.btnTransferir.Location = new System.Drawing.Point(133, 268);
+            this.btnTransferir.Margin = new System.Windows.Forms.Padding(2);
             this.btnTransferir.Name = "btnTransferir";
-            this.btnTransferir.Size = new System.Drawing.Size(112, 57);
+            this.btnTransferir.Size = new System.Drawing.Size(84, 46);
             this.btnTransferir.TabIndex = 0;
             this.btnTransferir.Text = "&Transferir";
             this.btnTransferir.UseVisualStyleBackColor = true;
@@ -59,9 +60,10 @@
             // 
             // btnExtrato
             // 
-            this.btnExtrato.Location = new System.Drawing.Point(359, 330);
+            this.btnExtrato.Location = new System.Drawing.Point(269, 268);
+            this.btnExtrato.Margin = new System.Windows.Forms.Padding(2);
             this.btnExtrato.Name = "btnExtrato";
-            this.btnExtrato.Size = new System.Drawing.Size(112, 57);
+            this.btnExtrato.Size = new System.Drawing.Size(84, 46);
             this.btnExtrato.TabIndex = 1;
             this.btnExtrato.Text = "&Extrato";
             this.btnExtrato.UseVisualStyleBackColor = true;
@@ -70,127 +72,162 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(155, 15);
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.Size = new System.Drawing.Size(50, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Pagador:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(142, 75);
+            this.label2.Location = new System.Drawing.Point(9, 61);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.Size = new System.Drawing.Size(62, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Remetente:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(609, 15);
+            this.label3.Location = new System.Drawing.Point(360, 12);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(63, 16);
+            this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Nº Conta:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(609, 78);
+            this.label4.Location = new System.Drawing.Point(360, 63);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.Size = new System.Drawing.Size(53, 13);
             this.label4.TabIndex = 5;
             this.label4.Text = "Nº Conta:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(174, 136);
+            this.label5.Location = new System.Drawing.Point(33, 110);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(42, 16);
+            this.label5.Size = new System.Drawing.Size(34, 13);
             this.label5.TabIndex = 6;
             this.label5.Text = "Valor:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(18, 182);
+            this.label7.Location = new System.Drawing.Point(9, 148);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(78, 16);
+            this.label7.Size = new System.Drawing.Size(62, 13);
             this.label7.TabIndex = 8;
             this.label7.Text = "Mensagem:";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(404, 136);
+            this.label8.Location = new System.Drawing.Point(206, 110);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(56, 16);
+            this.label8.Size = new System.Drawing.Size(46, 13);
             this.label8.TabIndex = 9;
             this.label8.Text = "Método:";
             // 
             // txtPagador
             // 
-            this.txtPagador.Location = new System.Drawing.Point(224, 12);
+            this.txtPagador.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPagador.Location = new System.Drawing.Point(71, 10);
+            this.txtPagador.Margin = new System.Windows.Forms.Padding(2);
             this.txtPagador.Name = "txtPagador";
-            this.txtPagador.Size = new System.Drawing.Size(364, 22);
+            this.txtPagador.Size = new System.Drawing.Size(274, 20);
             this.txtPagador.TabIndex = 10;
             // 
             // txtRemetente
             // 
-            this.txtRemetente.Location = new System.Drawing.Point(224, 72);
+            this.txtRemetente.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRemetente.Location = new System.Drawing.Point(71, 58);
+            this.txtRemetente.Margin = new System.Windows.Forms.Padding(2);
             this.txtRemetente.Name = "txtRemetente";
-            this.txtRemetente.Size = new System.Drawing.Size(364, 22);
+            this.txtRemetente.Size = new System.Drawing.Size(274, 20);
             this.txtRemetente.TabIndex = 11;
             // 
-            // textBox1
+            // txtContaPagador
             // 
-            this.textBox1.Location = new System.Drawing.Point(678, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 12;
+            this.txtContaPagador.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtContaPagador.Location = new System.Drawing.Point(411, 10);
+            this.txtContaPagador.Margin = new System.Windows.Forms.Padding(2);
+            this.txtContaPagador.Name = "txtContaPagador";
+            this.txtContaPagador.Size = new System.Drawing.Size(76, 20);
+            this.txtContaPagador.TabIndex = 12;
             // 
-            // textBox2
+            // txtContaRemetente
             // 
-            this.textBox2.Location = new System.Drawing.Point(678, 73);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 13;
+            this.txtContaRemetente.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtContaRemetente.Location = new System.Drawing.Point(411, 59);
+            this.txtContaRemetente.Margin = new System.Windows.Forms.Padding(2);
+            this.txtContaRemetente.Name = "txtContaRemetente";
+            this.txtContaRemetente.Size = new System.Drawing.Size(76, 20);
+            this.txtContaRemetente.TabIndex = 13;
             // 
-            // numericUpDown1
+            // numValorTransferencia
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(224, 134);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
-            this.numericUpDown1.TabIndex = 14;
+            this.numValorTransferencia.DecimalPlaces = 2;
+            this.numValorTransferencia.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numValorTransferencia.Location = new System.Drawing.Point(71, 109);
+            this.numValorTransferencia.Margin = new System.Windows.Forms.Padding(2);
+            this.numValorTransferencia.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numValorTransferencia.Name = "numValorTransferencia";
+            this.numValorTransferencia.Size = new System.Drawing.Size(90, 20);
+            this.numValorTransferencia.TabIndex = 14;
+            this.numValorTransferencia.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
-            // comboBox1
+            // comboBoxMetodoPagamento
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(467, 133);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 15;
+            this.comboBoxMetodoPagamento.FormattingEnabled = true;
+            this.comboBoxMetodoPagamento.Items.AddRange(new object[] {
+            "Pix Copia e Cola",
+            "Pix QR Code",
+            "Pix CPF/CNPJ"});
+            this.comboBoxMetodoPagamento.Location = new System.Drawing.Point(253, 108);
+            this.comboBoxMetodoPagamento.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxMetodoPagamento.Name = "comboBoxMetodoPagamento";
+            this.comboBoxMetodoPagamento.Size = new System.Drawing.Size(92, 21);
+            this.comboBoxMetodoPagamento.TabIndex = 15;
             // 
-            // textBox3
+            // txtMensagem
             // 
-            this.textBox3.Location = new System.Drawing.Point(21, 211);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(639, 83);
-            this.textBox3.TabIndex = 16;
+            this.txtMensagem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMensagem.Location = new System.Drawing.Point(11, 171);
+            this.txtMensagem.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMensagem.Multiline = true;
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(480, 68);
+            this.txtMensagem.TabIndex = 16;
             // 
             // frmPix
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(790, 434);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(500, 353);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.comboBoxMetodoPagamento);
+            this.Controls.Add(this.numValorTransferencia);
+            this.Controls.Add(this.txtContaRemetente);
+            this.Controls.Add(this.txtContaPagador);
             this.Controls.Add(this.txtRemetente);
             this.Controls.Add(this.txtPagador);
             this.Controls.Add(this.label8);
@@ -202,11 +239,11 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnExtrato);
             this.Controls.Add(this.btnTransferir);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmPix";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "frmPix";
-            this.Load += new System.EventHandler(this.frmPix_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numValorTransferencia)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,10 +262,10 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox txtPagador;
         private System.Windows.Forms.TextBox txtRemetente;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtContaPagador;
+        private System.Windows.Forms.TextBox txtContaRemetente;
+        private System.Windows.Forms.NumericUpDown numValorTransferencia;
+        private System.Windows.Forms.ComboBox comboBoxMetodoPagamento;
+        private System.Windows.Forms.TextBox txtMensagem;
     }
 }
--- Banco/frmPix.cs
+++ Banco/frmPix.cs
@@ -23,17 +23,61 @@ namespace Banco
 
         private void btnTransferir_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(txtPagador.Text) ||
+                string.IsNullOrWhiteSpace(txtRemetente.Text) ||
+                string.IsNullOrWhiteSpace(txtContaPagador.Text) ||
+                string.IsNullOrWhiteSpace(txtContaRemetente.Text) ||
+                numValorTransferencia.Value <= 0)
+            {
+                MessageBox.Show("Preencha todos os campos corretamente e informe um valor maior que zero.\n(A mensagem é opcional)");
+                return;
+            }
 
+            try
+            {
+                string pagador = txtPagador.Text.Trim();
+                string remetente = txtRemetente.Text.Trim();
+                int contaPagador = int.Parse(txtContaPagador.Text);
+                int contaRemetente = int.Parse(txtContaRemetente.Text);
+                decimal valor = numValorTransferencia.Value;
+                string mensagem = string.IsNullOrWhiteSpace(txtMensagem.Text) ? "Sem mensagem" : txtMensagem.Text.Trim();
+                string formaPagamento = comboBoxMetodoPagamento.SelectedItem?.ToString() ?? "Não informado";
+                string metodoPagamento = comboBoxMetodoPagamento.SelectedItem?.ToString() ?? "Não informado";
+
+                Lancamento lancamento = new Lancamento(pagador, remetente, contaPagador, contaRemetente, valor, mensagem, metodoPagamento, formaPagamento);
+                historicoLancamentos.Add(lancamento);
+
+                MessageBox.Show("Pix realizado com sucesso!\n" + lancamento.ToString());
+
+                txtPagador.Clear();
+                txtRemetente.Clear();
+                txtContaPagador.Clear();
+                txtContaRemetente.Clear();
+                txtMensagem.Clear();
+                numValorTransferencia.Value = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
         }
 
         private void btnExtrato_Click(object sender, EventArgs e)
         {
+            if (historicoLancamentos.Count == 0)
+            {
+                MessageBox.Show("Nenhum Pix realizado.");
+                return;
+            }
 
-        }
+            StringBuilder sb = new StringBuilder();
+            foreach (var lanc in historicoLancamentos)
+            {
+                sb.AppendLine(lanc.ToString());
+                sb.AppendLine(new string('-', 50));
+            }
 
-        private void frmPix_Load(object sender, EventArgs e)
-        {
-
+            MessageBox.Show(sb.ToString(), "Extrato Pix");
         }
     }
 }
